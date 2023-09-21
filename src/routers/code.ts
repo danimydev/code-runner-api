a7f33dda94684710281fc859d6642ed167dee707
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-09-21 11:10:16 -0500

    cleaning up code router

--- src/routers/code.ts
+++ src/routers/code.ts
@@ -1,33 +1,42 @@
 import { Router } from 'oak';
 import { runCode } from '../code-runner/run-code.ts';
+import { LANGUAGUES } from '../code-runner/languages.ts';
 
 export const codeRouter = new Router({ prefix: '/code' })
 	.post('/', async (ctx) => {
 		try {
 			if (!ctx.request.hasBody) {
-				ctx.throw(415);
+				ctx.throw(415, 'json body is required');
 			}
+
 			const reqBody = await ctx.request.body({ type: 'json' }).value as {
 				language: string;
 				code: string;
 			};
 
-			if (!reqBody.language) {
+			if (!reqBody.language || reqBody.language.trim().length === 0) {
 				ctx.throw(400, 'language is required');
 			}
 
-			if (!reqBody.code) {
+			if (!reqBody.code || reqBody.code.trim().length === 0) {
 				ctx.throw(400, 'code is required.');
 			}
 
-			const { code, decoded } = await runCode({
+			if (!LANGUAGUES.includes(reqBody.language)) {
+				ctx.throw(400, 'language not supported');
+			}
+
+			const { code, decoded: { stderr, stdout } } = await runCode({
 				language: reqBody.language,
 				codeText: reqBody.code,
 			});
 
+			ctx.response.status = 201;
 			return ctx.response.body = {
 				languague: reqBody.language,
-				...{ code, ...decoded },
+				code,
+				stdout,
+				stderr,
 				timeStampt: Date.now(),
 			};
 		} catch (error) {
