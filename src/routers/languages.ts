69fe911e782e45fe9d7e15a39247bdab5fc327a9
Daniel Madrid <31113489+danimydev@users.noreply.github.com>
2023-09-24 08:17:24 -0500

    languages router - redis enabled

--- src/routers/languages.ts
+++ src/routers/languages.ts
@@ -1,8 +1,20 @@
 import { Router } from 'oak';
 import { LANGUAGUES, LANGUAGUES_INFO_MAP } from '../code-runner/languages.ts';
+import { r2d2Wrapper } from '../redis/r2d2-wrapper.ts';
 
 export const languagesRouter = new Router({ prefix: '/languages' })
 	.get('/', async (ctx) => {
+		const cached = await r2d2Wrapper.get('languages');
+
+		if (cached) {
+			const formattedLanguages = JSON.parse(cached.toString());
+			ctx.response.status = 200;
+			return ctx.response.body = {
+				languages: formattedLanguages,
+				timeStampt: Date.now(),
+			};
+		}
+
 		const formattedLanguages = await Promise.all(
 			LANGUAGUES.map(async (
 				language,
@@ -40,6 +52,9 @@ export const languagesRouter = new Router({ prefix: '/languages' })
 			}),
 		);
 
+		await r2d2Wrapper.set('languages', JSON.stringify(formattedLanguages));
+		await r2d2Wrapper.setTTL('languages', 12 * 60 * 60);
+
 		ctx.response.status = 200;
 		return ctx.response.body = {
 			languages: formattedLanguages,
@@ -49,6 +64,17 @@ export const languagesRouter = new Router({ prefix: '/languages' })
 	.get('/:languageName', async (ctx) => {
 		try {
 			const { languageName } = ctx.params;
+
+			const cached = await r2d2Wrapper.get(`languages-${languageName}`);
+
+			if (cached) {
+				const enviromentInfo = JSON.parse(cached.toString());
+				ctx.response.status = 200;
+				return ctx.response.body = {
+					language: enviromentInfo,
+					timeStampt: Date.now(),
+				};
+			}
 
 			const languageInfo = LANGUAGUES_INFO_MAP.get(languageName);
 			if (!languageInfo) {
@@ -82,6 +108,12 @@ export const languagesRouter = new Router({ prefix: '/languages' })
 				};
 				return;
 			}
+
+			await r2d2Wrapper.set(
+				`languages-${languageName}`,
+				JSON.stringify(enviromentInfo),
+			);
+			await r2d2Wrapper.setTTL(`languages-${languageName}`, 12 * 60 * 60);
 
 			ctx.response.status = 200;
 			ctx.response.body = {
