83ebb0eed011dec10ea2ef2a0321b749dd17ddef
Daniel Madrid <danimydev@gmail.com>
2024-01-13 19:42:10 -0500

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import { Application } from "oak";
+import { oakCors } from "cors";
 
 import { healthRouter } from "@/routers/health.ts";
 import { languagesRouter } from "@/routers/languages.ts";
 import { codeRouter } from "@/routers/code.ts";
 
 const app = new Application()
+  .use(oakCors({ origin: "*" }))
   .use(healthRouter.routes())
   .use(languagesRouter.routes())
   .use(codeRouter.routes());
